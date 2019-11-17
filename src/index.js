aeee1e575a36273dfbd50cbeff426e4d0bf8d2ab
Jiahuan He <jiahuan_he@outlook.com>
2019-11-17 12:20:42 -0600

    make Element class

--- src/index.js
+++ src/index.js
@@ -26,7 +26,54 @@ const stock = {
     fill: 'white'
 }
 
-class Rect extends React.Component {
+// const makeDraggable = (elementID, x, y, callback) => {
+//     const id = elementID
+//     let draggable = document.getElementById(id);
+//     let offset
+//     draggable.addEventListener('mousedown', mouseDown)
+//     draggable.addEventListener('mouseup', mouseUpOrLeave)
+//     draggable.addEventListener('mouseleave', mouseUpOrLeave)
+
+//     function mouseDown(e) {
+//         console.log("mouse down")
+//         draggable = document.getElementById(id);
+//         offset = getMousePosition(e);
+//         offset.x -= parseFloat(draggable.getAttributeNS(null, "x"));
+//         offset.y -= parseFloat(draggable.getAttributeNS(null, "y"));
+//         draggable.addEventListener('mousemove', mouseMove)
+//     }
+
+
+//     // console.log(this.props)
+
+//     const mouseMove = (e) => {
+//         console.log("mouse move")
+//         if (!draggable) return
+//         e.preventDefault();
+//         const coord = getMousePosition(e);
+//         console.log(coord.x - offset.x)
+//         console.log(coord.y - offset.y)
+//         // updatePosition(id, coord.x - offset.x, coord.y - offset.y)
+//         callback(id, coord.x - offset.x, coord.y - offset.y)
+//         draggable.setAttributeNS(null, "x", x);
+//         draggable.setAttributeNS(null, "y", y);
+//     }
+
+//     function mouseUpOrLeave(e) {
+//         console.log("mouse up")
+//         draggable = null
+//     }
+
+//     function getMousePosition(e) {
+//         const CTM = draggable.getScreenCTM();
+//         return {
+//             x: (e.clientX - CTM.e) / CTM.a,
+//             y: (e.clientY - CTM.f) / CTM.d
+//         };
+//     }
+// }
+
+class Element extends React.Component {
     componentDidMount() {
         this.makeDraggable()
     }
@@ -35,11 +82,8 @@ class Rect extends React.Component {
         const id = this.props.stockID
         let draggable = document.getElementById(id);
         let offset
-        draggable.addEventListener('mousedown', mouseDown)
-        draggable.addEventListener('mouseup', mouseUpOrLeave)
-        draggable.addEventListener('mouseleave', mouseUpOrLeave)
 
-        function mouseDown(e) {
+        const mouseDown = (e) => {
             console.log("mouse down")
             draggable = document.getElementById(id);
             offset = getMousePosition(e);
@@ -47,9 +91,6 @@ class Rect extends React.Component {
             offset.y -= parseFloat(draggable.getAttributeNS(null, "y"));
             draggable.addEventListener('mousemove', mouseMove)
         }
-
-
-        console.log(this.props)
 
         const mouseMove = (e) => {
             console.log("mouse move")
@@ -64,37 +105,59 @@ class Rect extends React.Component {
             draggable.setAttributeNS(null, "y", this.props.y);
         }
 
-        function mouseUpOrLeave(e) {
+        const mouseUpOrLeave = (e) => {
             console.log("mouse up")
             draggable = null
         }
 
-        function getMousePosition(e) {
+        const getMousePosition = (e) => {
             const CTM = draggable.getScreenCTM();
             return {
                 x: (e.clientX - CTM.e) / CTM.a,
                 y: (e.clientY - CTM.f) / CTM.d
             };
         }
+
+        draggable.addEventListener('mousedown', mouseDown)
+        draggable.addEventListener('mouseup', mouseUpOrLeave)
+        draggable.addEventListener('mouseleave', mouseUpOrLeave)
     }
 
     render() {
+        return (
+            <g 
+                x={this.props.x} 
+                y={this.props.y} 
+                id={this.props.stockID} 
+                style={moveable}
+            >
+                <rect x={this.props.x} y={this.props.y} width={"5%"} height={"5%"} style={stock}/>
+                <foreignObject x={this.props.x} y={this.props.y} width="50" height="50">
+                    <div>test</div>
+                </foreignObject>    
+            </g>       
+        );
+    }
+}
+
+class Board extends React.Component {    
+    
+    render() {
+        const stocks = this.props.stockIDs.map(id => {
+            return <Element
+                key={id}
+                stockID={id}
+                x={this.props.stockPos[id].x}
+                y={this.props.stockPos[id].y}
+                updatePosition={this.props.updatePosition}
+            />
+        })
 
         return (
-            <div style={svgWrapper}>       
+            <div style={svgWrapper}>
                 <svg style={boardStyle}>
-                    <g 
-                        x={this.props.x} 
-                        y={this.props.y} 
-                        id={this.props.stockID} 
-                        style={moveable}
-                    >                        
-                        <rect x={this.props.x} y={this.props.y} width={"5%"} height={"5%"} style={stock}/>
-                        <foreignObject x={this.props.x} y={this.props.y} width="50" height="50">
-                            <div>test</div>
-                        </foreignObject>    
-                    </g>       
-                </svg>                         
+                    {stocks}
+                </svg>
             </div>
         );
     }
@@ -116,7 +179,7 @@ class Background extends React.Component {
         const posRef = firebase.database().ref('stockPos');
 
         IDRef.on('value', (stockIDs) => {
-            // console.log(stockIDs.val())
+
             this.setState({
                 stockIDs: stockIDs.val(),
             })
@@ -147,27 +210,21 @@ class Background extends React.Component {
     updatePosition = (stockID, x, y) => {
         let newPos = Object.assign({}, this.state.stockPos)
         newPos[stockID] = { x: x, y: y }
+        console.log("updatePositin: x: ", x, " y: ",y)
+        console.log(this.state.stockPos["stock0"])
         firebase.database().ref('stockPos').set({
             [stockID]: { x: x, y: y, },
         });
     }
 
     render() {
-        const stocks = this.state.stockIDs.map(id => {
-            return <Rect
-                key={id}
-                stockID={id}
-                x={this.state.stockPos[id].x}
-                y={this.state.stockPos[id].y}
-                updatePosition={this.updatePosition}
-                testFunc={this.testFunc}
-            />
-        })
 
         return (
-            <div>
-                {stocks}
-            </div>
+            <Board 
+                stockIDs={this.state.stockIDs}
+                stockPos={this.state.stockPos}
+                updatePosition={this.updatePosition}
+                ></Board>
         );
     }
 }
