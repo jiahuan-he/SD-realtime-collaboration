3b468f96adc04a8ad6a6e81fad9650a51e5d4089
Jiahuan He <jiahuan_he@outlook.com>
2020-03-30 20:52:56 -0600

    modify simulate.js so it can handle simulation with parameters

--- src/util/simulate.js
+++ src/util/simulate.js
@@ -1,6 +1,6 @@
 const { evaluate } = require('mathjs')
     
-export default (stocks, flows, timeFrom, timeTo, timeStep, stocksToSimulate, XAxisDataKey) => {
+export default (stocks, flows, parameters, timeFrom, timeTo, timeStep, stocksToSimulate, XAxisDataKey) => {
     console.log(stocks)
     console.log(flows)
     console.log(stocksToSimulate)
@@ -9,9 +9,14 @@ export default (stocks, flows, timeFrom, timeTo, timeStep, stocksToSimulate, XAx
         const arr = equation.replace(/ /g,'').split(/([\+\-\*\/^])/g)
         for(let i=0; i<arr.length; i++){            
             if(/^[a-z0-9]+$/i.test(arr[i]) && isNaN(arr[i])){
-                const foundStock = stocks.find( (stock) => stock.id === arr[i])
-                const foundFlow = flows.find( (flow) => flow.id === arr[i])
+                const foundParamter = parameters.find( (parameter) => parameter.name === arr[i])                
+                if(foundParamter){
+                    arr[i] = +foundParamter.value
+                    continue
+                }
                 
+                const foundStock = stocks.find( (stock) => stock.id === arr[i])
+                const foundFlow = flows.find( (flow) => flow.id === arr[i])                
                 const foundVar = foundStock ? foundStock:foundFlow
                 const len = res[foundVar.id].length
                 arr[i] = res[foundVar.id][len-1]
