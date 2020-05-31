9ba0440a64c856f5248b8c91d5077dc463b70f96
Jiahuan He <jiahuan_he@outlook.com>
2020-05-31 17:14:55 -0600

    extract logic of updatePosition to optimize code structure and change react components to functional wherever possible
    1. merge multiple update*position functions to one function by extracting the logic 2. change Cloud.js, Stock.js, and Parameter.js to functional component to reduce code length and achieve better performance 3. update UML file to reflect the change in code

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -18,71 +18,12 @@ const moveable = {
     cursor: 'move',
 }
 
-export default class Stock extends React.Component {
-    componentDidMount() {
-        this.makeDraggable()
-    }
-
-    makeDraggable = () => {
-        const id = this.props.stock.id
-        let draggable = document.getElementById(id);
-        let offset
-
-        const mouseDown = (e) => {
-            console.log("mouse down")
-            draggable = document.getElementById(id);
-            offset = getMousePosition(e);
-            offset.x -= parseFloat(draggable.getAttributeNS(null, "x"));
-            offset.y -= parseFloat(draggable.getAttributeNS(null, "y"));
-            draggable.addEventListener('mousemove', mouseMove)
-        }
-
-        const mouseMove = (e) => {
-            console.log("mouse move")
-            if (!draggable) return
-            e.preventDefault();
-            const coord = getMousePosition(e);
-            console.log(coord.x - offset.x)
-            console.log(coord.y - offset.y)
-            this.props.updatePosition(id, coord.x - offset.x, coord.y - offset.y)
-        }
-
-        const mouseUpOrLeave = (e) => {
-            console.log("mouse up")
-            draggable = null
-        }
-
-        // mouse position to svg position
-        const getMousePosition = (e) => {
-            const CTM = draggable.getScreenCTM();
-            return {
-                x: (e.clientX - CTM.e) / CTM.a,
-                y: (e.clientY - CTM.f) / CTM.d
-            };
-        }
-
-        draggable.addEventListener('mousedown', mouseDown)
-        draggable.addEventListener('mouseup', mouseUpOrLeave)
-        draggable.addEventListener('mouseleave', mouseUpOrLeave)
-    }
-
-    render() {
-        const x=this.props.stock.posX
-        const y=this.props.stock.posY
-        const id=this.props.stock.id
-        const initValue=this.props.stock.initValue
-        return (
-            <g 
-                x={x} 
-                y={y} 
-                id={id} 
-                style={moveable}
-            >
-                <rect x={x} y={y} width={"5%"} height={"5%"} style={this.props.highlight?stockHighlight:stock}/>
-                <foreignObject x={x} y={y} width="50" height="50">
-                    <div>{id+": "+initValue}</div>                 
-                </foreignObject>    
-            </g>       
-        );
-    }
-}
+export default ({posX, posY, id, initValue, highlight}) => 
+(
+    <g x={posX} y={posY} id={id} style={moveable}>
+        <rect x={posX} y={posY} width={"5%"} height={"5%"} style={highlight?stockHighlight:stock}/>
+        <foreignObject x={posX} y={posY} width="50" height="50">
+            <div>{id+": "+initValue}</div>                 
+        </foreignObject>    
+    </g>
+);
